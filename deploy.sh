12762c95e7b8f6701c59f0c4e4e203972933cb8c
Akash Rawal <argmlac@gmail.com>
2018-01-27 11:10:38 +0530

    Prepare for stable release

--- deploy.sh
+++ deploy.sh
@@ -5,6 +5,9 @@ if test "$CI_COMMIT_REF_NAME" = "master"; then
 	#Nightly release
 	version="nightly"
 	prefix="nightly-`date +"%Y%m%d-%H%M%S"`/"
+elif test "${CI_COMMIT_REF_NAME:0:8}" = "release-"; then
+	version="${CI_COMMIT_REF_NAME:8}"
+	prefix="release-$version"
 fi
 
 
@@ -23,6 +26,13 @@ upload()
 	package="$2"
 	remotefile="$3"
 
+	if test "$version" != "nightly"; then
+		data="{ \"name\" : \"$version\""
+		data="$data , \"desc\" : \"Stable release $version\" }"  
+		curl -X POST -d "$data" -u"$BINTRAY_KEY" \
+			"$BINTRAY_PROJECT/$package/versions"
+	fi
+	
 	curl -T "$localfile" -u"$BINTRAY_KEY" \
 		"$BINTRAY_PROJECT/$package/$version/${prefix}${remotefile}?publish=1"
 	curl -X PUT -u"$BINTRAY_KEY" \
@@ -30,12 +40,17 @@ upload()
 		-d "{ \"list_in_downloads\" : true }"
 }
 
+#curl()
+#{
+#	echo curl "$@"
+#}
+
 echo "Deploying to $BINTRAY_PROJECT, version $version, path $prefix"
 if test "$CI_JOB_NAME" = "mingw-w64"; then
 	upload "src/dispatch-ng.exe" "mingw-w64" "dispatch-ng.exe"
 elif test "$CI_JOB_NAME" = "linux"; then
 	filename="`ls | grep 'dispatch_ng.*\.tar\.gz'`"
-	upload "$filename" "source" "dispatch_ng.tar.gz"
+	upload "$filename" "source" "$filename"
 else
 	echo "No deployment from $CI_JOB_NAME"
 fi
