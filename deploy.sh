3a00896138a53346ca963cd7c8504f6767e9910e
Akash Rawal <argmlac@gmail.com>
2021-05-06 10:57:50 +0530

    Change bintray deployment to gitlab packages

--- deploy.sh
+++ deploy.sh
@@ -10,16 +10,19 @@ elif test "${CI_COMMIT_REF_NAME:0:8}" = "release-"; then
 	prefix="release-$version/"
 fi
 
-
-if test -z "$BINTRAY_PROJECT" || test -z "$BINTRAY_KEY"; then
-	echo "Project variables not set, skipping deployment"
-	exit
-fi
-
 if test -z "$version"; then
 	echo "Cannot deduce suitable version, skipping deployment"
 	exit
 fi
+
+upload_root="$CI_API_V4_URL/projects/$CI_PROJECT_PATH/packages/generic"
+
+curl_auth() {
+	#Print out curl commandline but hide the job token"
+	echo ">> curl $*" >&2
+	curl --header "JOB-TOKEN: $CI_JOB_TOKEN" "$@"
+	return $?
+}
 
 upload()
 {
@@ -27,26 +30,12 @@ upload()
 	package="$2"
 	remotefile="$3"
 
-	if test "$version" != "nightly"; then
-		data="{ \"name\" : \"$version\""
-		data="$data , \"desc\" : \"Stable release $version\" }"  
-		curl -X POST -d "$data" -u"$BINTRAY_KEY" \
-			"$BINTRAY_PROJECT/$package/versions"
-	fi
-	
-	curl -T "$localfile" -u"$BINTRAY_KEY" \
-		"$BINTRAY_PROJECT/$package/$version/${prefix}${remotefile}?publish=1"
-	curl -X PUT -u"$BINTRAY_KEY" \
-		"${BINTRAY_PROJECT/content/file_metadata}/${prefix}${remotefile}" \
-		-d "{ \"list_in_downloads\":true }"
+	curl_auth -T "$localfile"  \
+		"$upload_root/$package/$version/$remotefile"
 }
 
-#curl()
-#{
-#	echo curl "$@"
-#}
 
-echo "Deploying to $BINTRAY_PROJECT, version $version, path $prefix"
+echo "Deploying to $upload_root, version $version, path $prefix"
 if test "$CI_JOB_NAME" = "mingw-w64"; then
 	upload "src/dispatch-ng.exe" "mingw-w64" "dispatch-ng.exe"
 elif test "$CI_JOB_NAME" = "linux"; then
